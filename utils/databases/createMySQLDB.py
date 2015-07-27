d458098880438246679ede28bdd43c67d66075c2
dleecefft <dleece@firstfiretech.ca>
2015-07-26 21:31:58 -0600

    building the scripts to make lists and process them for by the clients

--- utils/databases/createMySQLDB.py
+++ utils/databases/createMySQLDB.py
@@ -10,7 +10,7 @@ def dbCreate():
 
     try:
         dbcon = mdb.connect('localhost',dbAdmin,dbAdminPWD,'mysql')
-        print "connected"
+        #print "connected"
 
     except mdb.Error, e:
         print e.args[0]
@@ -29,7 +29,7 @@ def dbCreate():
 def dbUserCreate():
     try:
         dbcon = mdb.connect('localhost',dbAdmin,dbAdminPWD,'mysql')
-        print "connected"
+        #print "connected"
 
     except mdb.Error, e:
         print e.args[0]
@@ -47,6 +47,28 @@ def dbUserCreate():
     dbcon.close()
     var = 'Create Database User done'
     return var
+def dbUserGrant():
+    try:
+        dbcon = mdb.connect('localhost',dbAdmin,dbAdminPWD,'mysql')
+        #print "connected"
+
+    except mdb.Error, e:
+        print e.args[0]
+        sys.exit(1)
+
+    with dbcon:
+        cur=dbcon.cursor()
+        cur.execute("USE dnsminerWA")
+        SQLstring = "GRANT all dnsminerWA.* TO 'dnsMinion'@'localhost'"
+        print SQLstring
+        cur.execute (SQLstring)
+        #cur.execute("CREATE TABLE IF NOT EXISTS ccircip(Id INT PRIMARY KEY auto_increment,\
+        #IPINT INT UNSIGNED, LASTUPDATE DATE)")
+    dbcon.commit()
+    dbcon.close()
+    var = 'Create Database User Permissions  done'
+    return var
 print "Debug"
 print dbCreate()
 print dbUserCreate()
+print dbUserGrant()
