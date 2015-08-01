9cbc2debf133338b8462b89122ad8cd5b06bb951
dleecefft <dleece@firstfiretech.ca>
2015-08-01 17:40:23 -0600

    building the scripts to make lists and process them for by the clients

--- bin/pitbossLFconfig.py
+++ bin/pitbossLFconfig.py
@@ -2,27 +2,29 @@
 #__author__ = 'dleece'
 import sys
 
-FEFQDN=""
-FEPORT=""
-SSLCert=""
+#FEFQDN=""
+#FEPORT=""
+#SSLCertPath=""
 MIConf="no"
 def minInput():
     FEFQDN = raw_input("Enter the FQDN for the Find Evil server, EG fe2.dnsminer.net: ")
     FEPort = raw_input("Enter the logstash receiver TCP/IP port, EG  2112: ")
     print "Enter the path to the SSL public cert copied from FE server"
-    SSLCert = raw_input( "EG, /var/lib/logstash-forwarder/keys/fePub.crt : ")
+    SSLCertPath = raw_input( "EG, /var/lib/logstash-forwarder/keys/fePub.crt : ")
+    minInputVals = [FEFQDN,FEPORT,SSLCertPath]
+    return minInputVals
 
-def confMinInput():
+def confMinInput(miList):
     print "confirm settings"
-    print "FQDN of Find Evil server: " + FEFQDN
-    print "TCP/IP port for Logstash on FE server: " + FEPORT
-    print "The full path to the FE public SSL cert: " + SSLCert
+    print "FQDN of Find Evil server: " + miList[0]
+    print "TCP/IP port for Logstash on FE server: " + miList[1]
+    print "The full path to the FE public SSL cert: " + miList[2]
 
 
 while True:
     if MIConf=='yes':
         break
-    minInput()
-    confMinInput()
+    thisList=minInput()
+    confMinInput(thisList)
     RI=raw_input("Are these settings correct?  yes/no:")
     MIConf=str.lower(RI.strip())
