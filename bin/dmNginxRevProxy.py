f639a9d69747f7d40312341f1fbdc4d3d6d9116b
dleecefft <dleece@firstfiretech.ca>
2015-08-02 21:05:07 -0600

    building the scripts to make lists and process them for by the clients

--- bin/dmNginxRevProxy.py
+++ bin/dmNginxRevProxy.py
@@ -3,6 +3,8 @@
 import sys, os, time, shutil
 # functions
 
+nginxbin = '/usr/sbin/nginx'
+nginxdir = '/etc/nginx'
 
 def minInput():
     FEFQDN = raw_input("Enter the FQDN for the Find Evil server, EG fe2.dnsminer.net: ")
@@ -20,16 +22,49 @@ def isPathValid(pathStr):
     TBool = os.path.isdir(pathStr)
     return TBool
 
+def genConfig(valList):
+    # current file location when installing package from elastic search
+    CFG = nginxdir + "/sites-available/revproxynew"
+    CFGOrig = nginxdir + "/sites-enabled/default"
+    if isPathValid(CFGOrig):
+        try:
+            os.unlink(CFGOrig)
+            print "Symlink to "+ CFGOrig + " has been removed"
+        except:
+            print " unable to remove symfile"
+            cStatus = "Check file permissions"
+    # Rather than mess with the package version which could change over time just write from scratch
+    CFGfh = open(CFG,'a')
+    # build the config
+    wline = "server {\n"
+    wline = wline + "  server_name " + valList[0] + ";\n"
+    wline = wline + "    listen " + valList[1] + " default ssl;\n"
+    wline = wline + "    ssl_certificate  /etc/nginx/local/dnsminer.crt;\n"
+    wline = wline + "    ssl_certificate_key /etc/nginx/local/dnsminerpriv.key;\n"
+    wline = wline + "    access_log /var/log/nginx/nginx.access.log combined;\n"
+    wline = wline + "    error_log /var/log/nginx/nginx_error.log notice;\n\n"
+    wline = wline + "location / {\n"
+    wline = wline + "  include proxy.conf;\n"
+    wline = wline + "  auth_basic  \" Restricted to authorized users only\"\n"
+    wline = wline + "  auth_basic_user_file /etc/nginx/local/user_auth;\n"
+    wline = wline + " proxy_pass http://127.0.0.1:" + valList[2] + ";\n"
+    wline = wline + " }\n}\n"
+    CFGfh.write(wline)
+    CFGfh.close()
+    CFGfh = open(CFG,'r')
+    for line in CFGfh:
+        print line
 
 
 
-if isFileValid('/usr/sbin/nginx') and isPathValid('/etc/nginx'):
+if isFileValid(nginxbin) and isPathValid(nginxdir):
     print "looks good, Nginx is installed "
 else:
     print "Nginx does not appear to be installed"
     quit()
 
 conflist = minInput()
-
 for items in conflist:
     print items
+
+genConfig(conflist)
