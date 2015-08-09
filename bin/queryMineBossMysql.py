9244797525a53dafb3a8b19cd6be19aa88fd8c6c
dleecefft <dleece@firstfiretech.ca>
2015-08-09 16:42:39 -0600

    building the scripts to make lists and process them for by the clients

---
+++ bin/queryMineBossMysql.py
@@ -0,0 +1,166 @@
+#!/usr/bin/env python
+#__author__ = 'dleece'
+import sys
+import string
+import ConfigParser
+import bcrypt
+from itertools import izip
+
+# noinspection PyUnresolvedReferences
+import MySQLdb as mdb
+
+DNSMinerHome='/opt/dnsminer-alpha'
+dbUtilsHome = DNSMinerHome + '/utils/databases/'
+dbcfg= DNSMinerHome + "/etc/dbConnections.cfg"
+
+
+def ConfigSectionMap(section):
+    dbcfgdict = {}
+    cfgoptions = dbconnect.options(section)
+    for cfgoption in cfgoptions:
+        try:
+            dbcfgdict[cfgoption] = dbconnect.get(section, cfgoption)
+            if dbcfgdict[cfgoption] == -1:
+                print "invalid parameter" + cfgoption
+        except:
+            print ('exception thrown, on %s' % cfgoption)
+            dbcfgdict[cfgoption] = None
+    return  dbcfgdict
+
+
+def userLogin():
+    credtest=False
+    while not credtest:
+        orgContact = raw_input("Enter your org contact email  : ")
+        orgContact = inputSanitizer(orgName,'emailstring')
+        orgPasswd = raw_input("Enter org admin's password : ")
+        orgPasswd = inputSanitizer(orgPasswd,'password')
+        credlist= [orgContact,orgPasswd]
+        credauthz = checkauthn(credlist) # return boolean for authenticated and org number ( honey token this?)
+        if not credauthz[0]:
+            print "credentail failure"
+
+    return credauthz
+
+def inputSanitizer(inputstring,type):
+    # sanitize based on whitelist and what type of input we're expecting
+    charwl = string.ascii_letters + string.whitespace + string.digits
+    if type == 'emailstring':
+        charwl = charwl + '@._-'
+    if type ==  'password':
+        charwl = string.printable
+
+    outstring = inputstring.strip()
+    tmpchar=''
+    for tchar in outstring:
+        if tchar not in charwl:
+            print "replacing invalid character " + tchar
+            tchar = '_'
+        tmpchar = tmpchar + tchar
+    outstring = tmpchar
+    return outstring
+
+def checkauthn(checkinput):
+    print "checking credentials supplied"
+    # by default config parser converts keys to lowercase , https://docs.python.org/2/library/configparser.html
+    adminVar= ConfigSectionMap("SectionOne")['databaseuser']
+    adminPwd= ConfigSectionMap("SectionOne")['databasepwd']
+    ivDBName= ConfigSectionMap("SectionOne")['databasename']
+    contactEmail = checkinput[0]
+    clearpasswd = checkinput[1]
+
+    authzlist = [False,8287]
+    try:
+        dbcon = mdb.connect('localhost',adminVar,adminPwd,ivDBName)
+        #print "connected"
+    except mdb.Error, e:
+        print e.args[0]
+        sys.exit(1)
+
+    with dbcon:
+        cur=dbcon.cursor()
+        sqlStr = "USE " + ivDBName
+        cur.execute(sqlStr)
+        sqlStr = "SELECT pwd from org_info WHERE org_contact = '" + contactEmail +"';"
+        print sqlStr
+        cur.execute(sqlStr)
+        storedpwd = cur.fetchone()[0]
+        print storedpwd
+        # pass the stoed hash to checkPW(st
+        testPwd = checkPwd(storedpwd,clearpasswd)
+        authzlist[0] = testPwd
+        if  authzlist[0]:
+            print "cool, you have a good set of creds "
+            # from here we'd generated a second query to grab the org_id and over write authzlist[1]
+    dbcon.commit()
+    dbcon.close()
+    return authzlist
+
+def createSQLInsertDict(inputvals):
+    # broke this out from the main org input collection to reduce clutter and allow debugging
+    iraw=iter(inputvals)
+    insertdict = dict(izip(iraw,iraw))
+    #for key, value in insertdict.iteritems():
+    #    print "Column: " + key
+    #    print "Value: " + value
+    return insertdict
+
+
+def dbTblInsert(insertdict,dbtable):
+    # by default config parser converts keys to lowercase , https://docs.python.org/2/library/configparser.html
+    adminVar= ConfigSectionMap("SectionOne")['databaseuser']
+    adminPwd= ConfigSectionMap("SectionOne")['databasepwd']
+    ivDBName= ConfigSectionMap("SectionOne")['databasename']
+    var = 'Record inserted successfully'
+
+    columnlist = []
+    valuelist = []
+    for key, value in insertdict.iteritems():
+        #print "Column: " + key
+        columnlist.append(key)
+        #print "Value: " + value
+        valuelist.append(value)
+    valstring ="','".join(valuelist)  # need the ticks for sql insert to work in mysql
+    colstring =",".join(columnlist)
+    sqlStrI = "INSERT INTO " + dbtable + "(" + colstring +") VALUES ('" + valstring +"');"
+    #print sqlStrI
+    try:
+        dbcon = mdb.connect('localhost',adminVar,adminPwd,ivDBName)
+        #print "connected"
+    except mdb.Error, e:
+        print e.args[0]
+        sys.exit(1)
+
+    with dbcon:
+        cur=dbcon.cursor()
+        sqlStr = "USE " + ivDBName
+        cur.execute(sqlStr)
+        #for sqlStr in fh:
+        #    if sqlStr.strip():
+        #        #print sqlStr
+        cur.execute (sqlStrI)
+    dbcon.commit()
+    dbcon.close()
+    return var
+def checkPwd(storedpwd,clearpasswd):
+    pwdtest = storedpwd==bcrypt.hashpw(clearpasswd,storedpwd)
+    return pwdtest
+def genBcrpytHash(plainString):
+    hashedpwd=bcrypt.hashpw(plainString,bcrypt.gensalt(14))
+    return hashedpwd
+# --- main -----------------------------------
+
+#readConfigIni(dbcfg)  ( convert to function )
+dbconnect=ConfigParser.ConfigParser()
+dbconnect.read(dbcfg)
+
+# gather org input, outputs a boolean and if true and org_id
+loginresult=userLogin()
+#
+#userMenu() # allow user to modify and update various fields
+# All database changes done via functions
+# Parse input array into SQL return a dictionary
+#dbinsertdict=createSQLInsertDict(orginfoinputs)
+
+# feed dictionary into sql insert
+#dbTblInsert(dbinsertdict,'org_info')
