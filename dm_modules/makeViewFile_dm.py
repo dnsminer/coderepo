07e7ba369fd0d961b288269926dfb03d641e4871
dleecefft <dleece@firstfiretech.ca>
2015-08-16 19:35:55 -0600

    building the scripts to make lists and process them for by the clients

--- dm_modules/makeViewFile_dm.py
+++ dm_modules/makeViewFile_dm.py
@@ -9,17 +9,18 @@ def readDict(thisorgdict):
         print key,"-->",val
         vfile = thisorgdict['view_name'] + ".view"
 
-    thisfh= getfilehandle(vfile)
-    for key,val in thisorgdict.iteritems():
-        line = " ( or { or // " + key + " more stuff \n" + val + "} } \n"
-        thisfh.write(line)
-    thisfh.close()
-    return
-
-def getfilehandle(fname):
-    thisfile = tempdir + fname
+    thisfile = tempdir + vfile
+    print thisfile
     try:
         fh = open(thisfile,'w')
+        #thisfh= getfilehandle(vfile)
+        for key,val in thisorgdict.iteritems():
+            line = " ( or { or // " + key + " more stuff \n" + val + "} } \n"
+            fh.write(line)
     except:
         print "Unable to create view file in temp directory, please debug"
-        return fh
+        return
+
+
+    fh.close()
+    return
