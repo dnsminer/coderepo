fc127936283b7ad9699ad38a733f722f05f062ff
dleecefft <dleece@firstfiretech.ca>
2015-08-22 01:24:02 -0600

    building the scripts to make lists and process them for by the clients

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from distutils.core import setup
 
 setup(
-    name='DNSMiner Modules',
+    name='DNS Miner Modules',
     version='0.5',
     packages=['dm_modules'],
     url='www.dnsminer.net',
     license='Apache License 2.0',
     author='Doug Leece',
     author_email='dleece@firstfiretech.ca',
-    description='Modules needed for the various DNS Miner programs and utilities'
+    description='Modules needed by various programs and utilities within the DNS Miner application'
 )
