d0730f43abdf4d3bff2355b4dd1099daa208f94f
dleecefft <dleece@firstfiretech.ca>
2015-08-23 13:27:49 -0600

    building the scripts to make lists and process them for by the clients

---
+++ dm_modules/dbselectSubqueryExclude_dm.py
@@ -0,0 +1,57 @@
+__author__ = 'dleece'
+
+import sys
+import MySQLdb as mdb
+import cfgparse_dm, inputSani_dm
+
+DNSMinerHome='/opt/dnsminer-alpha'
+dbcfg= DNSMinerHome + "/etc/dbConnections.cfg"
+
+def dbRecordSelect(selectinput):
+    thisCfgDict = cfgparse_dm.opencfg(dbcfg,'SectionOne')
+    adminVar = thisCfgDict['databaseuser']
+    adminPwd= thisCfgDict['databasepwd']
+    ivDBName = thisCfgDict['databasename']
+    #debug
+    print " sanitizing input data"
+    selectvalue1 = str(selectinput[0])
+    selectvalue1 = inputSani_dm.inputSanitizer(selectvalue1,'sqlval')
+    selecttable = str(selectinput[1])
+    selecttable = inputSani_dm.inputSanitizer(selecttable,'sqlval')
+    firsttable = str(selectinput[2])
+    firsttable = inputSani_dm.inputSanitizer(firsttable,'sqlval')
+    subval = str(selectinput[3])
+    subval = inputSani_dm.inputSanitizer(subval,'sqlval')
+    subtable = str(selectinput[4])
+    subtable = inputSani_dm.inputSanitizer(subtable,'sqlval')
+    wherecol = str(selectinput[5])
+    wherecol = inputSani_dm.inputSanitizer(wherecol,'sqlval')
+    whereval = str(selectinput[6])
+    whereval = inputSani_dm.inputSanitizer(whereval,'sqlval')
+
+    #debug
+    print "retrieving data"
+    resultlist = []
+    try:
+        dbcon = mdb.connect('localhost',adminVar,adminPwd,ivDBName)
+        #print "connected"
+    except mdb.Error, e:
+        print e.args[0]
+        sys.exit(1)
+
+    with dbcon:
+        cur=dbcon.cursor()
+        sqlStr = "USE " + ivDBName
+        cur.execute(sqlStr)
+        sqlStr = "SELECT " + selectvalue1 + " from " + selecttable + " where " + firsttable + " NOT in ( SELECT "\
+                 + subval + " FROM " + subtable + " WHERE " + wherecol + " = '" + whereval +"';"
+
+        cur.execute(sqlStr)
+        rows = cur.fetchall()
+        # Make use we got at least one record
+        if rows[0] is not None:
+            for row in rows:
+                resultlist.append(row)
+    dbcon.commit()
+    dbcon.close()
+    return resultlist
--- utils/dailyOrgRPZ.py
+++ utils/dailyOrgRPZ.py
@@ -3,25 +3,76 @@ __author__ = 'dleece'
 # Set the path to include the dns miner modules directory
 import sys
 import MySQLdb as mdb
-from dm_modules import cfgparse_dm, bulkdbselect1w_dm,bulkdbselectJoin1w_dm
+from dm_modules import cfgparse_dm, bulkdbselect1w_dm,bulkdbselectJoin1w_dm, dbselectSubqueryExclude_dm
 
 DNSMinerHome='/opt/dnsminer-alpha'
 dbcfg= DNSMinerHome + "/etc/siteSpecific.cfg"
+rpzbase = getrpzbase()
 
-def getViewIDOrg(oidlist):
+
+def genRPZFiles(oidlist):
     # use the list of distinct org ids, search for all views per org id
     # extract view name and sink hole id, use sh_id to get sh_fqdn
     # pass org_id,view_name,def_sh_fqdn to a function that writes out an RPZ file to a specific directory to
     for item in oidlist:
         if item:
             orgid=item[0]
+            # get org wide, dom list,
+            # remove white list entries from public list
+
+            # add black list entries, store as a temp list pubwhiteblackdomlist
+            makeorgpwblist(orgid)
+            # Get view metadata
             viewmdata = getViewRPZdata(orgid)
-            #
-            for rows in viewmdata:
-                print "--> Org ID " + str(orgid) + " view name, view ID, sh fqdn"
-                for i in range(len(rows)):
-                    print rows[i]
+            # Debug
+            #for rows in viewmdata:
+            #    print "--> Org ID " + str(orgid) + " view name, view ID, sh fqdn"
+            #    for i in range(len(rows)):
+            #        print rows[i]
+
+            # Generate view specific header
+
+            # Open file handle, use rpzbase/orgid/view.rpz as path,
+            # write header
+            # iterate through pubwhiteblackdomlist and add view specific sh_fqdn as cname per line
+            # rpz should have dom cname fqdn.  and *.dom cname fqdn.  ( end dots important)
+
+            #close file handle,
+            # do next view
+            # if no more views go to next org id
+
+
+
+
     return
+
+
+def makeorgpwblist(oid):
+    pubwhtblk = []
+    # Remove any white list domains from public list and write to temp list
+    selstr = "tlist_domains.domain"
+    stbl = "tlist_domains"
+    sval = "tlist_domains.domain"
+    sval2 = "wl_domain"
+    stbl2 = "whitelist_domain"
+    wval = "whitelist_domain.org_id"
+    slctlist=[selstr,stbl,sval,sval2,stbl2,wval,oid]
+    pubwhtblk = dbselectSubqueryExclude_dm.dbRecordSelect(selstr)
+    # debug
+    print "public minus white list :" + str(len(pubwhtblk))
+    # select black list doms and append to list
+    selstr = "bl_domain"
+    stbl = "blacklist_domains"
+    slctlist = [selstr,stbl,oid]
+    tmpblk = bulkdbselect1w_dm.dbRecordSelect(slctlist)
+    if tmpblk[0]:
+        for val in tmpblk:
+            pubwhtblk.append(val[0])
+    print "public minus white list  plust black:" + str(len(pubwhtblk))
+
+
+    return pubwhtblk
+
 
 def getViewRPZdata(oid):
     # use Org ID in sql join with where to get data for rpz needed by each view
@@ -65,8 +116,15 @@ def getOrgID():
     dbcon.close()
     return oidrows
 
+
+def getrpzbase():
+    thisCfgDict = cfgparse_dm.opencfg(dbcfg,'SectionThree')
+    dirbase = thisCfgDict['rpzbase']
+    return dirbase
+
 def main():
     thisoidlist=getOrgID()
-    getViewIDOrg(thisoidlist)
+
+    genRPZFiles(thisoidlist)
 
 if __name__ == "__main__": main()
