7baf086a75f9cbe2de9e8970081eb6a0a3351ea6
dleecefft <dleece@firstfiretech.ca>
2015-08-22 21:44:00 -0600

    building the scripts to make lists and process them for by the clients

--- utils/dailyOrgRPZ.py
+++ utils/dailyOrgRPZ.py
@@ -9,15 +9,17 @@ DNSMinerHome='/opt/dnsminer-alpha'
 dbcfg= DNSMinerHome + "/etc/siteSpecific.cfg"
 
 def getViewIDOrg(oidlist):
-
+    # use the list of distinct org ids, search for all views per org id
+    # extract view name and sink hole id, use sh_id to get sh_fqdn
+    # pass org_id,view_name,def_sh_fqdn to a function that writes out an RPZ file to a specific directory to
     for item in oidlist:
         if item:
             orgid=item[0]
-            selstr = "view_id,view_name,def_sh_id"
+            selstr = "view_name,def_sh_id"
             slctlist=[selstr,'bind_views','org_id',orgid]
             allorgViews = bulkdbselect_dm.dbRecordSelect(slctlist)
             for rows in allorgViews:
-                print "--------------------"
+                print "--------" + str(orgid) + "view name, sinkhole ID"
                 for i in range(len(rows)):
                     print rows[i]
 
