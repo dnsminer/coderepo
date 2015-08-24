d3d1440dbc7b9b7b36520817c5b700de4ca79047
dleecefft <dleece@firstfiretech.ca>
2015-08-23 18:46:42 -0600

    building the scripts to make lists and process them for by the clients

--- dm_modules/genViewACL_dm.py
+++ dm_modules/genViewACL_dm.py
@@ -4,18 +4,29 @@ __author__ = 'dleece'
 import bulkdbselectJoin1wExclude_dm
 #acl firstfireACL { !key mineboss-underground-hosting; key mineboss-firstfire; 104.37.193.75; 45.58.36.25; 104.131.209.101; };
 
+DNSMinerHome='/opt/dnsminer-alpha'
 # use viewID to get teh not keys and return list of key names
 def genACL(viewdict):
     thisvid=viewdict['view_id']
     thisacl = viewdict['acl_name']
-    aclstr = "acl " + thisacl + "( "
+    thiskey = viewdict['tsig_name']
+    thisrnode = viewdict['rec_nodes']
+    thisvname = viewdict['view_name']
+    aclstr = "acl " + thisacl + " ( "
     exkeylist=getexcludekeys(thisvid)
     if exkeylist[0]:
         for val in exkeylist:
             print val[0]
-            aclstr = aclstr + "!key " + val[0] + ";"
-
+            aclstr = aclstr + "!key " + val[0] + "; "
+    # add the valid key
+    aclstr = aclstr + " key " + thiskey + "; "
+    rnlist=thisrnode.split(',')
+    for i in range(len(rnlist)):
+        if rnlist[i]:
+            aclstr = aclstr + rnlist[i] + "; "
+    aclstr = aclstr + " };"
     print aclstr
+    writeACL(thisvname,aclstr)
 
     return
 
@@ -32,4 +43,20 @@ def getexcludekeys(vid):
     excludekeys = bulkdbselectJoin1wExclude_dm.dbRecordSelect(slctlist)
     return excludekeys
 
+def writeACL(vname,astr):
+    tdir = DNSMinerHome + "/tmp/"
+    fname = tdir + vname + ".viewacl"
+    try:
+        fh = open(fname,'w')
+        #for line in linelist:
+        line = astr + "\n"
+        fh.write(line)
+    except Exception as e:
+        print "Unable to create view file in temp directory or problem with the file content, please debug"
+        print type(e)
+        print str(e)
+        return
+    fh.close()
+
+    return
 
