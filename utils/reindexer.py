6c2228bccb03fcdcd7141cbe92099993b7f91815
dleecefft <dleece@firstfiretech.ca>
2015-09-05 16:02:20 -0600

    building the scripts to make lists and process them for by the clients

--- utils/reindexer.py
+++ utils/reindexer.py
@@ -18,17 +18,11 @@ DNSMinerHome='/opt/dnsminer-alpha'
 dbcfg= DNSMinerHome + "/etc/siteSpecific.cfg"
 
 def unicodelinesplitter(uline):
-    retlist = []
     aline = str(uline)
+    aline = aline.strip()
     rowlist = aline.split()
-    for i in range(len(rowlist)):
-        retlist.append(rowlist[i])
+    return rowlist
 
-    return retlist
-
-
-
-client = Elasticsearch([{'host':'localhost','port':9200}], sniff_on_start=True, sniff_on_connection_fail=True)
 #query={"query": {"match_all" : {}}}
 #scanResp = client.search(index="logstash-2015.08.11", doc_type=["DNSQRY","PDNS"], body=query, search_type="scan", scroll="10m", size=10000)
 #for key,val in scanResp.iteritems():
@@ -36,13 +30,48 @@ client = Elasticsearch([{'host':'localhost','port':9200}], sniff_on_start=True, 
 #scrollId=scanResp['_scroll_id']
 #response = client.scroll(scroll_id=scrollId, scroll="10m")
 
-response = client.cat.indices( index='logstash-*', h=['index','docs.count'])
 
+def getsourceindexs(srcprefix):
+# get list of non-empty source indexs to be passed to reindexer function
+# assuming we are using prefix-date stamp since this tends to be the autocreate default
+    indexlist = []
+    srxidx = srcprefix + "*"
+    client = Elasticsearch([{'host':'localhost','port':9200}], sniff_on_start=True, sniff_on_connection_fail=True)
+    indexcat = client.cat.indices( index=srxidx, h=['index','docs.count'])
 
+# The result comes out as a unicode string, needed to do a little slicing and dicing to create a list to return
+    icatlist = indexcat.splitlines()
+    for line in icatlist:
+        resultlist = unicodelinesplitter(line)
+        if resultlist[1] > 0:
+            indexlist.append(resultlist[0])
+    return indexlist
 
-responselist = response.splitlines()
-for line in responselist:
-    printlist = unicodelinesplitter(line)
-    for i in range(len(printlist)):
-        print str(i) + str(printlist[i])
+def doreindex(srcidxlist,dstidx):
+    print "reindexing to " + dstidx
+    for row in srcidxlist:
+        print row[0]
 
+def reindexmenu():
+    doindexing=True
+    while doindexing:
+        print "\nYou are about to reindex a potentially large amount of data, this could take a while"
+        print "\nWhat is the prefix of the source indexes? EG, logstash-2015.08.31 would be logstash"
+        while doindexing:
+            uidxinput = raw_input("Enter source index prefix: ")
+            uidxinput = uidxinput.strip().lower()
+            ilist = getsourceindexs(uidxinput)
+            if len(ilist) > 0:
+                print "great, looks like we have " + str(len(ilist)) + "indices to reindex"
+            else:
+                print "sorry, that doesn't look like a valid index prefix"
+                print "try running a command like this curl -GET http://localhost:9200/_cat/indices?v from the FE server command prompt to display the index names"
+                continue
+
+            uidxinput = raw_input("Enter destination index name: ")
+            uidxinput = uidxinput.strip().lower()
+
+            # This is where we could write a create index with mapping function but for now we're just going to assume
+            # the index is ready waiting and properly configured.
+            doreindex(ilist,uidxinput)
+            doindexing = False
