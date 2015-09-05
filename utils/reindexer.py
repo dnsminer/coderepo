323c70e9051f1ad837fd9ef75f9fe89aa4d1d344
dleecefft <dleece@firstfiretech.ca>
2015-09-05 14:17:29 -0600

    building the scripts to make lists and process them for by the clients

--- utils/reindexer.py
+++ utils/reindexer.py
@@ -10,21 +10,22 @@ from datetime import date, datetime
 from elasticsearch import Elasticsearch
 from elasticsearch_dsl import Search, Q
 from elasticsearch import helpers
+from elasticsearch.helpers import reindex
 
 ## site specific settings
 DNSMinerHome='/opt/dnsminer-alpha'
 dbcfg= DNSMinerHome + "/etc/siteSpecific.cfg"
 
 client = Elasticsearch([{'host':'localhost','port':9200}], sniff_on_start=True, sniff_on_connection_fail=True)
-query={"query": {"match_all" : {}}}
-scanResp = client.search(index="logstash-2015.08.11", doc_type=["DNSQRY","PDNS"], body=query, search_type="scan", scroll="10m", size=10000)
+#query={"query": {"match_all" : {}}}
+#scanResp = client.search(index="logstash-2015.08.11", doc_type=["DNSQRY","PDNS"], body=query, search_type="scan", scroll="10m", size=10000)
+#for key,val in scanResp.iteritems():
+#    print key, '-->', scanResp[key]
+#scrollId=scanResp['_scroll_id']
+#response = client.scroll(scroll_id=scrollId, scroll="10m")
 
-for key,val in scanResp.iteritems():
-    print key, '-->', scanResp[key]
-
-scrollId=scanResp['_scroll_id']
-response = client.scroll(scroll_id=scrollId, scroll="10m")
-
+response = client.search(index="logstash-2015.08.19", doc_type="DNSQRY", body={"query":{"match": {"View:"FirstFire"}}})
+print ("%d documents found" % response['hits']['total'])
 for hit in response["hits"]["hits"]:
     print hit
     print "next doc"
