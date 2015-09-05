4b82340a71ed9d1635a2230fc34b2a3bf86117fb
dleecefft <dleece@firstfiretech.ca>
2015-09-05 11:53:31 -0600

    building the scripts to make lists and process them for by the clients

---
+++ utils/reindexer.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+__author__ = 'dleece'
+# Set the path to include the dns miner modules directory
+import sys, os
+import MySQLdb as mdb
+import sys
+import json
+from dm_modules import cfgparse_dm, bulkdbselect1w_dm,bulkdbselectJoin1w_dm, dbselectSubqueryExclude_dm
+from datetime import date, datetime
+from elasticsearch import Elasticsearch
+from elasticsearch_dsl import Search, Q
+
+## site specific settings
+DNSMinerHome='/opt/dnsminer-alpha'
+dbcfg= DNSMinerHome + "/etc/siteSpecific.cfg"
+
+client = Elasticsearch([{'host':'localhost','port':9200}])
+query={"query": {"match_all" : {}}}
+scanResp = client.search(index="logstash-2015.08.11", doc_type=["DNSQRY","PDNS"], body=query, search_type="scan", scroll="10m", size=10000)
+
+for key,val in scanResp.iteritems():
+    print key, '-->', scanResp[key]
+
+scrollId=scanResp['_scroll_id']
+response = client.scroll(scroll_id=scrollId, scroll="10m")
+
+for hit in response["hits"]["hits"]:
+    print hit
+    print "next doc
+
