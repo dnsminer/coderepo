0c2de293438199eddd7d0bc4b85a1c57e0ebb3d1
dleecefft <dleece@firstfiretech.ca>
2015-09-06 15:04:33 -0600

    building the scripts to make lists and process them for by the clients

---
+++ reporting/dailysumTldByType.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+__author__ = 'dleece'
+# Set the path to include the dns miner modules directory
+import sys, os
+import MySQLdb as mdb
+import string
+import click
+from dm_modules import cfgparse_dm, bulkdbselect1w_dm,bulkdbselectJoin1w_dm, dbselectSubqueryExclude_dm
+
+from elasticsearch import Elasticsearch
+from elasticsearch_dsl import Search, Q
+
+@click.command()
+@click.option('--vname',prompt='Viewname for report',help='Valid View name within elasticsearch, check Kibana discovery type:DNSQRY')
+@click.option('--lookback',default=10,help='Number of days, previous to today to include in report scope')
+
+def runreport(vname,lookback):
+    print "running teh report for " + vname
+    print "this will run " +str(lookback) + " times"
+    return
+
+
+if __name__ == '__main__':
+    runreport
---
+++ utils/cdncleaner.py
@@ -0,0 +1,93 @@
+#!/usr/bin/env python
+__author__ = 'dleece'
+# Set the path to include the dns miner modules directory
+import sys, os
+import MySQLdb as mdb
+import sys
+import json
+import string
+from dm_modules import cfgparse_dm, bulkdbselect1w_dm,bulkdbselectJoin1w_dm, dbselectSubqueryExclude_dm
+from datetime import date, datetime
+from elasticsearch import Elasticsearch
+from elasticsearch_dsl import Search, Q
+from elasticsearch import helpers
+from elasticsearch.helpers import reindex
+from elasticsearch.helpers import BulkIndexError
+
+
+## site specific settings
+DNSMinerHome='/opt/dnsminer-alpha'
+dbcfg= DNSMinerHome + "/etc/siteSpecific.cfg"
+
+def unicodelinesplitter(uline):
+    aline = str(uline)
+    aline = aline.strip()
+    rowlist = aline.split()
+    return rowlist
+
+#query={"query": {"match_all" : {}}}
+#scanResp = client.search(index="logstash-2015.08.11", doc_type=["DNSQRY","PDNS"], body=query, search_type="scan", scroll="10m", size=10000)
+#for key,val in scanResp.iteritems():
+#    print key, '-->', scanResp[key]
+#scrollId=scanResp['_scroll_id']
+#response = client.scroll(scroll_id=scrollId, scroll="10m")
+
+
+def getsourceindices(srcprefix):
+# get list of non-empty source indexs to be passed to reindexer function
+# assuming we are using prefix-date stamp since this tends to be the autocreate default
+    indexlist = []
+    srxidx = srcprefix + "*"
+    client = Elasticsearch([{'host':'localhost','port':9200}], sniff_on_start=True, sniff_on_connection_fail=True)
+    indexcat = client.cat.indices( index=srxidx, h=['index','docs.count'])
+# The result comes out as a unicode string, needed to do a little slicing and dicing to create a list to return
+    icatlist = indexcat.splitlines()
+    for line in icatlist:
+        resultlist = unicodelinesplitter(line)
+        if resultlist[1] > 0:
+            indexlist.append(resultlist[0])
+    return indexlist
+
+
+def doreindex(srcidxlist,dstidx):
+    print "reindexing to " + dstidx
+    client = Elasticsearch([{'host':'localhost','port':9200}], sniff_on_start=True, sniff_on_connection_fail=True)
+    for row in srcidxlist:
+        print "Reindexing index " + row + " to " + dstidx + " please stand by and watch for errors"
+        try:
+            reindex(client,row,dstidx,scroll='5m')
+        except BulkIndexError:
+            print "Problems with one or more documents from the source index above due to incompatable data types. They will not be included in the destination index"
+            sys.exc_clear()
+
+    print "Confirm the documents were all reindexed\nrun curl -GET http://localhost:9200/_cat/indices?v | grep " + dstidx
+    return
+
+
+def reindexmenu():
+    doindexing=True
+    while doindexing:
+        print "\nYou are about to reindex a potentially large amount of data, this could take a while"
+        print "\nWhat is the prefix of the source indexes? EG, logstash-2015.08.31 would be logstash"
+        while doindexing:
+            uidxinput = raw_input("Enter source index prefix: ")
+            uidxinput = uidxinput.strip().lower()
+            ilist = getsourceindices(uidxinput)
+            if len(ilist) > 0:
+                print "great, looks like we have " + str(len(ilist)) + " indices to reindex"
+            else:
+                print "sorry, that doesn't look like a valid index prefix"
+                print "try running a command like this \" curl -GET http://localhost:9200/_cat/indices?v from the FE server command prompt to display the index names\"\n"
+                continue
+
+            uidxinput = raw_input("Enter destination index name: ")
+            uidxinput = uidxinput.strip().lower()
+
+            # This is where we could write a create index with mapping function but for now we're just going to assume
+            # the index is ready waiting and properly configured.
+            doreindex(ilist,uidxinput)
+            doindexing = False
+
+# Main for running interactively
+if __name__ == "__main__":
+    reindexmenu()
