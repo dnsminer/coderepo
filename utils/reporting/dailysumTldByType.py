ab3bce81dfb4620a51690c4dbf8ac3f270039d61
dleecefft <dleece@firstfiretech.ca>
2015-09-06 23:44:24 -0600

    building the scripts to make lists and process them for by the clients

--- utils/reporting/dailysumTldByType.py
+++ utils/reporting/dailysumTldByType.py
@@ -94,7 +94,7 @@ def searchindexes(ilist,wname,wval,lb,dtype):
             print "Warning, no index found, report may not cover all days scoped"
             sys.exc_clear()
 
-        writereport(dnsHisto,wval)
+    writereport(dnsHisto,wval)
     return
 
 
