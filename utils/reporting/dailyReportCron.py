81c757baf1203014d0040660290f0d8bd4891619
dleecefft <dleece@firstfiretech.ca>
2015-09-10 20:22:54 -0600

    building the scripts to make lists and process them for by the clients

---
+++ utils/reporting/dailyReportCron.py
@@ -0,0 +1,125 @@
+#!/usr/bin/env python
+__author__ = 'dleece'
+# Set the path to include the dns miner modules directory
+import sys, os, markup, string
+#import MySQLdb as mdb
+#import string
+from dm_modules import cfgparse_dm, bulkdbselect1w_dm,bulkdbselectJoin1w_dm, dbselectSubqueryExclude_dm
+
+DNSMinerHome='/opt/dnsminer-alpha'
+sitecfg= DNSMinerHome + "/etc/siteSpecific.cfg"
+
+from elasticsearch import Elasticsearch
+from elasticsearch import helpers
+from elasticsearch.exceptions import NotFoundError
+from elasticsearch.helpers import scan
+
+
+
+
+def searchindexes(idxnamewq,wname,wval,lb,dtype):
+    # Get all the Views that have had some activity in the previous 10 days
+    daysback = "now-"+str(lb)+"d"
+    esclient = Elasticsearch([{'host':'localhost','port':9200}], sniff_on_start=True, sniff_on_connection_fail=True)
+    #histoList = list()
+    #dnsHisto = dict()
+    #dateHisto = dict()
+    #requests = 1
+
+    # Left big & open for troubleshooting syntax isses
+    qry = "{ \"query\": {\
+            \"filtered\" : {\
+                \"filter\": {\
+                        \"range\": { \"@timestamp\" : { \"gt\" : \"" + daysback + "\", \"lt\" : \"now\"}}\
+                        }\
+                }\
+        },\
+        \"aggs\": {\
+                \"ViewQueries\": {\
+                        \"" + wname + "\" : {\
+                                \"field\": \"" + wval + "\"\
+                                }\
+                        }\
+                }\
+}"
+    print qry
+    try:
+        response = esclient.search(index=idxnamewq, body=qry, doc_type=dtype)
+        for tag in response['aggregations']['ViewQueries']['buckets']:
+            print(tag['key'],tag['doc_count'])
+
+    except NotFoundError:
+        print "Warning, no index found, report may not cover all days scoped"
+        sys.exc_clear()
+
+    return
+
+
+
+
+
+
+
+def getreportparams():
+    thisCfgDict = cfgparse_dm.opencfg(sitecfg,'SectionThree')
+    rptbase = thisCfgDict['reportbase']
+    rptbase =  DNSMinerHome  + rptbase
+    return rptbase
+
+def genvhtml(flist,vpath):
+
+    title = "Daily reports for View"
+    header = "The CSV files below are generated daily, save a local copy and filter using the spreadsheet tool of your choice"
+    footer = "Don't forget to revist the Kibana discover application to do additional drill downs into anything from teh reports that piques your curiosity"
+    blank = "  "
+    page = markup.page()
+    page.init (title=title,header=header, footer=footer)
+    page.br()
+
+    for f in flist:
+        linkstr = "\"Report: " + f + "\", href='" + f + "' "
+        page.a(linkstr)
+    page.p(blank)
+    # write teh file
+    viewhtml = vpath + "/" + "view.html"
+    print viewhtml
+    htmlstr = str(page)
+    file2write=open(viewhtml,'w')
+    file2write.write(htmlstr)
+    file2write.close()
+    return
+
+
+
+def getviewlist(dirpath):
+    #Get all the directories that are not empty
+
+    viewlist = os.listdir(dirpath)
+    for fdir in viewlist:
+        filepaths = []
+        vdir = dirpath + "/" + fdir
+        print vdir
+        for dpath, dname, fnames in os.walk(vdir):
+            for fname in fnames:
+                filepaths.append(fname)
+            if len(filepaths) > 1:
+                genvhtml(filepaths,vdir)
+
+
+    return
+
+
+
+def genhtmlwrapper():
+    reppath = getreportparams()
+    vlist = getviewlist(reppath)
+
+
+    return
+
+
+
+
+if __name__ == '__main__':
+
+    searchindexes('dmlogstash2-*','terms','View','10','DNSQRY')
