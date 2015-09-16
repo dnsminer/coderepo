e3901e112a34df68dab269651391a899338ebdbf
dleecefft <dleece@firstfiretech.ca>
2015-09-15 23:05:39 -0600

    building the scripts to make lists and process them for by the clients

---
+++ contrib/fibRPZDailyTrans.sh
@@ -0,0 +1,9 @@
+#!/bin/bash -x
+# Define FE user, Usually fetchitBoy
+FEUSER="fetchitBoy"
+#Define FE host FQDN
+FEHOST="fe2.dnsminer.net"
+# Get teh RPZ file for today, ( check your cron and TZ settings if this fails )
+TODAYRPZ = $(/bin/date +%Y%m%d)-public-list.rpz
+
+/usr/bin/scp $(TODAYRPZ) $(FEUSER)@FEHOST:
